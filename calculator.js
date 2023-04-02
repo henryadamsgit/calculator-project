6f0d69627230883f58911b153429b55d0c308985
Henry Adams <henrybruce.adams@icloud.com>
2023-04-03 00:06:36 +0100

    Added functions for cancel and equals button in js file

--- calculator.js
+++ calculator.js
@@ -1,59 +1,72 @@
 // VARIABLES TO ACCESS ELEMENTS
 const numberButton = document.querySelectorAll("#numbers");
 const operatorButton = document.querySelectorAll("#operators");
-const cancelButton = document.querySelector("#cancel");
+const cancelButton = document.querySelector("#clear");
+const equalsButton = document.querySelector("#equals");
 const displayTotal = document.querySelector("#total");
-const displayCalculation = document.querySelector("#equals");
 
-// VARIABLES THAT STORE ALL NUMBERS AND ALL OPERATORS
-
-// GLOBAL SCOPE
+// GLOBAL SCOPE VARIABLES
 let number = "";
 let operator = "";
-let cancel = "";
 let total = "";
-
-// when button is clicked, store tezt that is in the button
 
 // FUNCTIONS
 
-const reverseString = (displayTotal) => {
-  return displayTotal.reverse().join();
-};
-
-//performs when a number is clicked
+// FUNCTION performed when a number is clicked
 const handleNumberClick = (event) => {
   const incomingNumber = event.target.innerText;
-  number = incomingNumber + number;
+  number = number + incomingNumber;
   displayTotal.innerText = number;
 };
 
-// when an operator is clicked
+// FUNCTION performed when an operator is clicked
 const handleOperatorClick = (event) => {
   const incomingOperator = event.target.innerText;
-  operator = number + incomingOperator;
+  operator = number;
+  number = "";
+  operator += incomingOperator;
   displayTotal.innerText = operator;
 };
 
-// when cancel is clicked
-const handleCancelClick = (event) => {
-  const incomingCancel = event.target.innerText;
+// FUNCTION performed when cancel is clicked
+const handleCancelClick = () => {
   displayTotal.innerText = "0";
+  number = "";
 };
 
-//
-//EVENT LISTENERS
-// when numbers are clicked
+// FUNCTION performed when equals button is clicked
+const handleEqualsClick = () => {
+  if (operator === "+") {
+    total = Number(total) + Number(number);
+  } else if (operator === "-") {
+    total = Number(total) - Number(number);
+  } else if (operator === "*") {
+    total = Number(total) * Number(number);
+  } else if (operator === "/") {
+    total = Number(total) / Number(number);
+  } else {
+    total = number;
+  }
+
+  displayTotal.innerText = total;
+  number = "";
+  operator = "";
+};
+
+// EVENT LISTENERS
+
+// LISTEN for when numbers are clicked
 numberButton.forEach((button) => {
   button.addEventListener("click", handleNumberClick);
 });
-//when operators are clicked
+// LISTEN for when operators are clicked
 operatorButton.forEach((button) => {
   button.addEventListener("click", handleOperatorClick);
 });
-// when cancel is clicked
-//cancelButton((button) => {
-// button.addEventListener("click", handleCancelClick);
-//});
 
-//when equals is clicked
+// LISTEN for when cancel is clicked
+cancelButton.addEventListener("click", handleCancelClick);
+
+// LISTEN forwhen equals is clicked
+
+equalsButton.addEventListener("click", handleEqualsClick);
